a0bc8f74da471c3b2e0a7d0e7d78ea200876a85f
Tom Charman <tom@thegreatescapism.com>
2022-03-06 10:07:42 +1100

    Avatars in API

--- API/ScorpioAPI/Controllers/UserController.cs
+++ API/ScorpioAPI/Controllers/UserController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using ScorpioData.Dtos;
 using ScorpioData.Services;
 using Microsoft.AspNetCore.Mvc;
+using System.IO;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,10 +15,12 @@ namespace ScorpioAPI.Controllers
     public class UserController : Controller
     {
         private readonly IUserService _userService;
+        private readonly IWebHostEnvironment _env;
 
-        public UserController(IUserService userService)
+        public UserController(IUserService userService, IWebHostEnvironment env)
         {
             _userService = userService;
+            _env = env;
         }
 
         [HttpGet("me")]
@@ -35,6 +38,25 @@ namespace ScorpioAPI.Controllers
                 return NotFound();
             }
             return Ok(user);
+        }
+
+        [HttpGet("{id}/avatar")]
+        public IActionResult GetUserAvatar(int id)
+        {
+            var user = _userService.GetUser(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var imagePath = Path.Combine(
+                _env.ContentRootPath,
+                $"Avatars/{user.Id}.jpg"
+            );
+
+            var image = PhysicalFile(imagePath, "image/jpeg");
+            return image;
         }
     }
 }
