25adbf008c6948458bca0142395f8476e311cc8e
Tom Charman <tom@thegreatescapism.com>
2022-03-06 10:08:18 +1100

    Avatars and comment styling

--- FrontEnd/public/javascripts/components.js
+++ FrontEnd/public/javascripts/components.js
@@ -1,3 +1,18 @@
+export const makeAvatar = (user) => {
+    return `<img class="avatar" src="https://localhost:7001/api/user/${user.id}/avatar" />`
+}
+
 export const makeComment = (comment) => {
-    return `<div>${comment.text} - </div>`
+    return `<div class="comment">
+        ${makeAvatar(comment.user)}
+        <div class="commentDetails">
+            <div class="commentHeader">
+                <span class="name">${comment.user.name}</span>
+                •
+                <span class="date">${comment.postedDate}</span>
+            </div>
+            <div class="commentText">${comment.text}</div>
+            <div class="commentVotes"></div>
+        </div>
+    </div>`
 }
